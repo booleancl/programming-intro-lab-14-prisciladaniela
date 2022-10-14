47be8fea232adb1c6d4272ea60b2bb65fe7b3f7a
Priscila <prisciladaniela25@hotmail.com>
2022-10-14 10:58:12 -0300

    cardinalidad de relacion

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,6 @@
+\c blog
+
+--\COPY users(email) FROM 'C:/Users/prisc.DESKTOP-RA320T3/intro_dev/programming-intro-lab-14-prisciladaniela/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/prisc.DESKTOP-RA320T3/intro_dev/programming-intro-lab-14-prisciladaniela/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/prisc.DESKTOP-RA320T3/intro_dev/programming-intro-lab-14-prisciladaniela/data/comentarios.csv' csv HEADER;
 
